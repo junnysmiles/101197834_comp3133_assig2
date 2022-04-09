3ef8d8e69f37e521e1dde3c5ff4b9575a61c4c93
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2022-04-08 23:47:49 -0400

    generated new listings list component, added toolbar in app html, added some styling components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingslistComponent } from './listingslist/listingslist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: ListingslistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/listingslist/listingslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listingslist',
+  templateUrl: './listingslist.component.html',
+  styleUrls: ['./listingslist.component.css']
+})
+export class ListingslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
